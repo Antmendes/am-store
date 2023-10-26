b027b62e7d27e4aed0a55811ce6392d61523559d
Antmendes <antoniomendesjunior02@gmail.com>
2023-10-26 10:59:43 -0300

    adicionando produtos no carrinho

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -8,6 +8,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
 import Link from "next/link";
+import Cart from "./cart";
 
 const Header = () => {
     const {status, data} = useSession()
@@ -97,9 +98,17 @@ const Header = () => {
               </h1>
             </Link>
 
-            <Button size="icon" variant="outline">
-                <ShoppingCart/>
-            </Button>
+            <Sheet>
+              <SheetTrigger>
+                <Button size="icon" variant="outline">
+                  <ShoppingCart/>
+                </Button>
+              </SheetTrigger>
+
+              <SheetContent>
+                <Cart />
+              </SheetContent>
+            </Sheet>
         </Card>
      );
 }
